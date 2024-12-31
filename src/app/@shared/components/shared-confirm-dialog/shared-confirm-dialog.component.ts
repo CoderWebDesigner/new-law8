f5749ca8d51f5f68648b78d8844dd69777b357b5
ahmed ramzy <Ahmedramzy_fcih@yahoo.com>
2024-12-31 23:26:42 +0200

    fix dynamic dialog overlay

--- src/app/@shared/components/shared-confirm-dialog/shared-confirm-dialog.component.ts
+++ src/app/@shared/components/shared-confirm-dialog/shared-confirm-dialog.component.ts
@@ -1,14 +1,12 @@
 import { Component, inject } from '@angular/core';
 import { SharedButtonComponent } from '../shared-button/shared-button.component';
-import { NgOptimizedImage } from '@angular/common';
 import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
 
 @Component({
   selector: 'app-shared-confirm-dialog',
   standalone: true,
   imports: [
-    SharedButtonComponent,
-    NgOptimizedImage
+    SharedButtonComponent
   ],
   templateUrl: './shared-confirm-dialog.component.html',
   styleUrl: './shared-confirm-dialog.component.scss'
